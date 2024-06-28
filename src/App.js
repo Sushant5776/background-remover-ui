995600ab9ae217010dc3212369448c141b483f00
Sushant Garudkar <garudkarsushant@yahoo.com>
2024-06-28 14:32:45 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+// import { saveAs } from 'file-saver';
 
-function App() {
+const ImageUpload = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [uploadStatus, setUploadStatus] = useState('');
+  const [imageURL, setImageURL] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const handleFileChange = (event) => {
+    setImageURL('')
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleFileUpload = async () => {
+    if (!selectedFile) {
+      setUploadStatus('No file selected');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', selectedFile);
+
+    setLoading(true)
+    
+    try {
+      const response = await axios.post('https://bg-remover-api-djvzob3udq-el.a.run.app/', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+        responseType: 'blob'
+      });
+      console.log(response)
+      const img_url = URL.createObjectURL(response.data)
+      setImageURL(img_url)
+      // saveAs(response.data, 'output.png')
+      setUploadStatus('Done!')
+      // setUploadStatus(`File uploaded successfully: ${response.data.filename}`);
+    } catch (error) {
+      setUploadStatus(`File upload failed: ${error.message}`);
+    }
+    setLoading(false)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{width: 'fit-content', margin: 'auto'}}>
+      <h1>Background Remover AI (Just name)</h1>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleFileUpload}>Upload</button>
+      <p>{uploadStatus}</p>
+      {selectedFile && <img width={256} style={{objectFit: 'contain'}} src={URL.createObjectURL(selectedFile)} alt='' />}
+      {
+        loading ? <p>Loading...</p> :
+       imageURL ? <img width={256} style={{objectFit: 'contain'}} src={imageURL} alt='Upload Image to See Output' /> : <span>Upload Image and See Output.</span>}
     </div>
   );
-}
+};
 
-export default App;
+export default ImageUpload;
